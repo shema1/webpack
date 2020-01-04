4cc445dbd1cb343b548912818554032d9435de18
unknown <shymechko.m.a@gmail.com>
2020-01-04 18:34:52 +0200

    commit

--- lesson_1/task_1/src/js/storeage.js
+++
@@ -1,9 +0,0 @@
-const setItem = (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-}
-
-function getItem(key) {
-    return JSON.parse(localStorage.getItem(key));
-};
-
-export { setItem, getItem }
