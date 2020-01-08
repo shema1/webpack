c16d6f1ea76434e563a0cd84688139968426356b
unknown <shymechko.m.a@gmail.com>
2020-01-08 18:07:05 +0200

    commit

---
+++ Lesson_4/task_1/calculator/index.js
@@ -0,0 +1,16 @@
+export const sum = (a, b) => {
+    console.log('implementation for sum');
+    // ... implementation
+};
+
+export const mult = (a, b) => {
+    console.log('implementation for mult');
+    // ... implementation
+};
+
+// экспортируйте calc как export по умолчанию
+
+export default (expression) => {
+    console.log('implementation for calc');
+    // ... implementation
+};
