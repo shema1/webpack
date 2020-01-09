62e9293873d765af32c1ed4a9adb8fd8727cf35f
unknown <shymechko.m.a@gmail.com>
2020-01-09 16:50:21 +0200

    commit

---
+++ Lesson_8/task_2/src/index.js
@@ -0,0 +1,9 @@
+import renderProfile from './profile/renderProfile.js';
+import './index.css';
+
+const profileData = {
+    name: 'Tom',
+    location: 'The World'
+};
+
+renderProfile(profileData);
--- Lesson_8/task_2/webpack.config.js
+++ Lesson_8/task_2/webpack.config.js
@@ -1,9 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const webpack = require('webpack');
+
+
 module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production'
+
+    const isProdaction = argv.mode === 'prodaction';
     const config = {
         entry: './src/index.js',
         output: {
@@ -11,12 +12,8 @@ module.exports = (env, argv) => {
         },
         module: {
             rules: [{
-                    test: /.js$/,
-                    use: ['babel-loader'],
-                },
-                {
                     test: /.s?css$/,
-                    use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader'],
+                    use: [isProdaction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader'],
                 },
                 {
                     test: /.(jpg|png)$/,
@@ -32,22 +29,16 @@ module.exports = (env, argv) => {
             ],
         },
         plugins: [
-            new webpack.ProgressPlugin(),
-            new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
                 template: './src/index.html'
             })
         ],
-        devServer: {
-            port: 9000,
-            hot: true,
-        }
     }
-
-    if (isProduction) {
+    if (isProdaction) {
         config.plugins.push(new MiniCssExtractPlugin({
             filename: '[name].css',
         }))
     }
+
     return config
 };
